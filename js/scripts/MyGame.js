48a02cdbc30669c5462cba9c8fda3a677a2c7f65
Sourav Ray <souravray@codewallas-Mac-mini-4.local>
2012-08-23 13:25:26 +0530

    Initial check-in

---
+++ js/scripts/MyGame.js
@@ -0,0 +1,13 @@
+
+// This is the constructor for the game
+MyGame = function()
+{
+    
+}
+
+// New methods and overrides for your game class will go in here
+MyGame.prototype =
+{
+
+}
+extend(MyGame, TGE.Game, null);
