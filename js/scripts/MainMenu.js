9d491ee6f5998f12643173e4fe5e68bcfab5ff84
ChetanGandhi <chetan.g@codewalla.com>
2012-08-26 22:27:51 +0530

    commit

--- js/scripts/MainMenu.js
+++ js/scripts/MainMenu.js
@@ -13,6 +13,8 @@ MainMenu.prototype.Setup = function()
     this.Game().mPauseButton.Hide();
     this.CreateUIEntity(TGE.ScreenEntity).Setup( this.mScreenManager.XFromPercentage(0.5), this.mScreenManager.YFromPercentage(0.5),
         "screen-background");
+    this.CreateUIEntity(TGE.ScreenEntity).Setup( this.mScreenManager.XFromPercentage(0.5), this.mScreenManager.YFromPercentage(0.2),
+        "game-name");
     this.CreateUIEntity(TGE.Button).Setup( this.mScreenManager.XFromPercentage(0.5), this.mScreenManager.YFromPercentage(0.5),
         "mainmenu_play_button", MainMenu.prototype.playGame.bind(this), 2, this.mScreenManager.mLayerName);
 }
